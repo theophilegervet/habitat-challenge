e8b4ccd89c12763e3946d55f546b641eb4f7a14b
theophilee <theophile.gervet@gmail.com>
2022-06-14 10:16:44 -0700

    Move frontier exploration to GPU

--- submission/policy/frontier_exploration_policy.py
+++ submission/policy/frontier_exploration_policy.py
@@ -2,9 +2,19 @@ from typing import Tuple, Optional
 import torch
 from torch import Tensor
 import skimage.morphology
-import numpy as np
 
 from .policy import Policy
+
+
+def binary_dilation(binary_image, kernel):
+    return torch.clamp(
+        torch.nn.functional.conv2d(
+            binary_image,
+            kernel,
+            padding=kernel.shape[-1] // 2
+        ),
+        0, 1
+    )
 
 
 class FrontierExplorationPolicy(Policy):
@@ -12,16 +22,36 @@ class FrontierExplorationPolicy(Policy):
     This policy picks the closest non-explored point.
     """
 
+    def __init__(self):
+        super().__init__()
+
+        self.dilate_explored_kernel = torch.from_numpy(
+            skimage.morphology.disk(10)
+        ).unsqueeze(0).unsqueeze(0).float()
+
+        self.select_border_kernel = torch.from_numpy(
+            skimage.morphology.disk(1)
+        ).unsqueeze(0).unsqueeze(0).float()
+
     def forward(self,
                 map_features: Tensor,
                 goal_category: Tensor
                 ) -> Tuple[Tensor, Optional[Tensor]]:
-        # TODO Move this to GPU
         batch_size, _, height, width = map_features.shape
-        map_features = map_features.cpu().numpy()
-        goal_category = goal_category.cpu().numpy()
+        device = map_features.device
 
-        goal_map = np.zeros((batch_size, height, width))
+        # Select unexplored area
+        frontier_map = (map_features[:, [1], :, :] == 0).float()
+
+        # Dilate explored area
+        frontier_map = 1 - binary_dilation(
+            1 - frontier_map, self.dilate_explored_kernel)
+
+        # Select the frontier
+        frontier_map = binary_dilation(
+            frontier_map, self.select_border_kernel) - frontier_map
+
+        goal_map = torch.zeros((batch_size, height, width), device=device)
 
         for e in range(batch_size):
             # If the object goal category is present in the local map, go to it
@@ -31,21 +61,7 @@ class FrontierExplorationPolicy(Policy):
 
             # Else, set unexplored area as the goal
             else:
-                goal_map[e] = map_features[e, 1, :, :] == 0
+                goal_map[e] = frontier_map[e]
 
-                goal_map[e] = 1 - skimage.morphology.binary_dilation(
-                    1 - goal_map[e],
-                    skimage.morphology.disk(10)
-                ).astype(int)
-
-                # Select the frontier
-                goal_map[e] = (
-                    skimage.morphology.binary_dilation(
-                        goal_map[e],
-                        skimage.morphology.disk(1)
-                    ).astype(int) - goal_map[e]
-                )
-
-        goal_map = torch.from_numpy(goal_map)
         regression_logits = None
         return goal_map, regression_logits
