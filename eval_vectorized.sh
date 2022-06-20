0ffe7448b06c9211e0cac4eb3f7b7943f71d50bb
theophilee <theophile.gervet@gmail.com>
2022-06-19 19:26:51 -0700

    Bash script to run multiple experiments in a row

--- eval_vectorized.sh
+++ eval_vectorized.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-python eval_vectorized.py \
-  EVAL_VECTORIZED.specific_category "potted plant" \
-  EXP_NAME potted_plant_through_bash_script
-
-python eval_vectorized.py \
-  EVAL_VECTORIZED.specific_category tv \
-  EXP_NAME tv_through_bash_script
+python eval_vectorized.py EVAL_VECTORIZED.specific_category "potted plant" EXP_NAME potted_plant_through_bash_script
+python eval_vectorized.py EVAL_VECTORIZED.specific_category tv EXP_NAME tv_through_bash_script
