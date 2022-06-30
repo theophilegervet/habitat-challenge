80b37cace47d6904a5ee63d27914e2e836a16e41
theophilee <theophile.gervet@gmail.com>
2022-06-29 18:06:54 -0700

    Learned exploration

--- submission/agent_module.py
+++ submission/agent_module.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import time
 
@@ -102,9 +103,10 @@ class AgentModule(nn.Module):
         # Predict high-level goals from map features
         # batched across sequence length x num environments
         map_features = seq_map_features.flatten(0, 1)
-        # TODO Compute orientation
+        orientation = torch.div(seq_global_pose.flatten(0, 1) % 360, 5,
+                                rounding_mode='trunc')
         print(seq_global_pose)
-        orientation = seq_global_pose.flatten(0, 1)
+        print(orientation)
         goal_category = seq_goal_category.flatten(0, 1)
         (
             goal_map,
