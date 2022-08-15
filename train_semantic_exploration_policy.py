f926ce809dddb11f951a92f8ddbdce9b121e51b4
Theophile Gervet <theophile.gervet@gmail.com>
2022-08-15 23:51:57 +0900

    dev

--- train_semantic_exploration_policy.py
+++ train_semantic_exploration_policy.py
@@ -11,7 +11,7 @@ from ray.rllib.agents import ppo
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.tune.logger import pretty_print
-from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray.rllib.evaluation import Episode
 
 from submission.utils.config_utils import get_config
