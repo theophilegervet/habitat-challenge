64b6dcfa3c73cdac3dfdd772f609502624c193b7
Theophile Gervet <theophile.gervet@gmail.com>
2022-08-17 19:31:17 +0900

    dev

--- debug.py
+++
@@ -1,7 +0,0 @@
-from train.semexp_policy_training_dataset import SemanticExplorationPolicyTrainingDataset
-import habitat
-from habitat.core.env import Env
-
-
-config = habitat.get_config("train/semexp_policy_training_env_config.yaml")
-env = Env(config)
