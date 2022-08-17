e3c729e51f8bdc8f0c3e30dcf218ae3eceb34944
Theophile Gervet <theophile.gervet@gmail.com>
2022-08-17 19:15:10 +0900

    dev

--- train/train_semantic_exploration_policy.py
+++ train/train_semantic_exploration_policy.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import warnings
 warnings.filterwarnings("ignore")
 import sys
+print(str(Path(__file__).resolve().parent))
 sys.path.insert(0, str(Path(__file__).resolve().parent))
 
 import ray
