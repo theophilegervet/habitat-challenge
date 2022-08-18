34b98c27a60017da4ad365be790f120a1f040181
theophilee <theophile.gervet@gmail.com>
2022-08-18 15:08:33 +0900

    dev

--- train/generate_semexp_policy_training_dataset.py
+++ train/generate_semexp_policy_training_dataset.py
@@ -96,9 +96,10 @@ for split in ["train"]:
 
     done_scene_keys = set([path.split("/")[-1].split(".")[0]
                            for path in f"{DATASET_ROOT_PATH}/FULL/{split}/scenes/*"])
-    todo_scenes = [scene for scene in scenes
-                   if scene.split("/")[-1].split(".")[0] not in done_scene_keys]
-    print(todo_scenes)
+    print(done_scene_keys)
+    # todo_scenes = [scene for scene in scenes
+    #                if scene.split("/")[-1].split(".")[0] not in done_scene_keys]
+    # print(todo_scenes)
     # with multiprocessing.Pool(80) as pool, tqdm.tqdm(total=len(scenes)) as pbar:
     #     for _ in pool.imap_unordered(generate_scene_episodes, scenes):
     #         pbar.update()
