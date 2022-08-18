f9cb8bca705f45b2c0fec035bc98e9f857557d0d
theophilee <theophile.gervet@gmail.com>
2022-08-18 15:16:24 +0900

    dev

--- train/generate_semexp_policy_training_dataset.py
+++ train/generate_semexp_policy_training_dataset.py
@@ -43,44 +43,41 @@ def generate_episode(sim, episode_count: int) -> ObjectGoalNavEpisode:
     )
 
 
-def generate_scene_episodes(scene_path: str, num_episodes: int = 5000):
-    try:
-        # 1K scenes * 5K episodes per scene = 5M episodes
-        if "train" in scene_path:
-            split = "train"
-        elif "val" in scene_path:
-            split = "val"
-        else:
-            raise ValueError
+def generate_scene_episodes(scene_path: str, num_episodes: int = 100):
+    # 800 train scenes * 5K episodes per scene = 4M train episodes
+    # 100 val scenes * 100 episodes per scene = 1K eval episodes
+    if "train" in scene_path:
+        split = "train"
+    elif "val" in scene_path:
+        split = "val"
+    else:
+        raise ValueError
 
-        config = habitat.get_config(str(
-            Path(__file__).resolve().parent.parent /
-            "submission/dataset/semexp_policy_training_env_config.yaml"
-        ))
-        config.defrost()
-        config.SIMULATOR.SCENE = scene_path
-        config.DATASET.SPLIT = split
-        config.freeze()
+    config = habitat.get_config(str(
+        Path(__file__).resolve().parent.parent /
+        "submission/dataset/semexp_policy_training_env_config.yaml"
+    ))
+    config.defrost()
+    config.SIMULATOR.SCENE = scene_path
+    config.DATASET.SPLIT = split
+    config.freeze()
 
-        sim = habitat.sims.make_sim("Sim-v0", config=config.SIMULATOR)
+    sim = habitat.sims.make_sim("Sim-v0", config=config.SIMULATOR)
 
-        # Create dataset and episodes
-        dataset = SemanticExplorationPolicyTrainingDataset(
-            config.DATASET, dataset_generation=True)
-        for episode_count in range(num_episodes):
-            dataset.episodes.append(generate_episode(sim, episode_count))
-        for ep in dataset.episodes:
-            ep.scene_id = ep.scene_id.split("scene_datasets/")[-1]
+    # Create dataset and episodes
+    dataset = SemanticExplorationPolicyTrainingDataset(
+        config.DATASET, dataset_generation=True)
+    for episode_count in range(num_episodes):
+        dataset.episodes.append(generate_episode(sim, episode_count))
+    for ep in dataset.episodes:
+        ep.scene_id = ep.scene_id.split("scene_datasets/")[-1]
 
-        # Store episodes with one file per scene
-        scene_key = scene_path.split("/")[-1].split(".")[0]
-        out_path = f"{DATASET_ROOT_PATH}/FULL/{split}/scenes/{scene_key}.json.gz"
-        os.makedirs(os.path.dirname(out_path), exist_ok=True)
-        with gzip.open(out_path, "wt") as f:
-            f.write(dataset.to_json())
-
-    except:
-        pass
+    # Store episodes with one file per scene
+    scene_key = scene_path.split("/")[-1].split(".")[0]
+    out_path = f"{DATASET_ROOT_PATH}/FULL/{split}/scenes/{scene_key}.json.gz"
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    with gzip.open(out_path, "wt") as f:
+        f.write(dataset.to_json())
 
 
 # Dataset is stored in per-scene files, generate empty split files
@@ -91,15 +88,11 @@ for split in ["val", "train"]:
         json.dump(dict(episodes=[]), f)
 
 # Generate per-scene files
-for split in ["train"]:
+for split in ["val"]:
     scenes = glob.glob(f"{SCENES_ROOT_PATH}/hm3d/{split}/*/*basis.glb")
 
-    done_scene_keys = set([path.split("/")[-1].split(".")[0]
-                           for path in glob.glob(f"{DATASET_ROOT_PATH}/FULL/{split}/scenes/*")])
-    todo_scenes = [scene for scene in scenes
-                   if scene.split("/")[-1].split(".")[0] not in done_scene_keys]
     # with multiprocessing.Pool(80) as pool, tqdm.tqdm(total=len(scenes)) as pbar:
     #     for _ in pool.imap_unordered(generate_scene_episodes, scenes):
     #         pbar.update()
-    for scene in tqdm.tqdm(todo_scenes):
+    for scene in tqdm.tqdm(scenes):
         generate_scene_episodes(scene)
