782fbe6dd28844a8d80acab59d73e0e876cccaaf
theophilee <theophile.gervet@gmail.com>
2022-08-19 21:39:15 +0900

    dev

--- submission/env_wrapper/semexp_policy_training_env_wrapper_temp_frontier_eval.py
+++ submission/env_wrapper/semexp_policy_training_env_wrapper_temp_frontier_eval.py
@@ -49,15 +49,17 @@ class SemanticExplorationPolicyTrainingEnvWrapper(RLEnv):
             if ALL_SCENES_MASK in config.TASK_CONFIG.DATASET.CONTENT_SCENES:
                 scenes = [dataset.scene_from_scene_path(scene_id)
                           for scene_id in dataset.scene_ids]
-            print("scenes", scenes)
+            print("scenes::::", scenes)
             del dataset
             scene_splits = [[] for _ in range(rllib_config.num_workers)]
             for idx, scene in enumerate(scenes):
                 scene_splits[idx % len(scene_splits)].append(scene)
             assert sum(map(len, scene_splits)) == len(scenes)
+            print("len(scene_splits)", len(scene_splits))
+            print("rllib_config.worker_index", rllib_config.worker_index)
             config.TASK_CONFIG.DATASET.CONTENT_SCENES = [
                 scene_splits[rllib_config.worker_index]]
-            print("scene_splits[rllib_config.worker_index]",
+            print("scene_splits[rllib_config.worker_index]::::",
                   scene_splits[rllib_config.worker_index])
 
             # Set random seed
