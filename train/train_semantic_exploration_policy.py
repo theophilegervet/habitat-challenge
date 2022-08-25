f7ed3aaa1c0973f3eef82cfe78b692101190a675
theophilee <theophile.gervet@gmail.com>
2022-08-25 11:55:35 +0900

    dev

--- train/train_semantic_exploration_policy.py
+++ train/train_semantic_exploration_policy.py
@@ -11,8 +11,10 @@ import sys
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 import ray
-from ray.rllib.algorithms import ppo, ddppo
-from ray.rllib.algorithms.callbacks import DefaultCallbacks
+# from ray.rllib.algorithms import ppo, ddppo
+# from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.agents import ppo, ddppo
+from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.evaluation import Episode
