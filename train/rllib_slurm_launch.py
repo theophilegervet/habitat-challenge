e50dc325957565ae645c126005f3d99001f1be46
theophilee <theophile.gervet@gmail.com>
2022-08-26 09:51:11 +0900

    Handle external redis

--- train/rllib_slurm_launch.py
+++ train/rllib_slurm_launch.py
@@ -1,12 +1,12 @@
 # Usage:
-python train/rllib_slurm_launch.py \
-  --exp-name train_semexp \
-  --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_debug_config.yaml" \
-  --load-env "" \
-  --num-nodes 1 \
-  --num-gpus 8 \
-  --partition learnfair \
-  --external-redis
+# python train/rllib_slurm_launch.py \
+#   --exp-name train_semexp \
+#   --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_debug_config.yaml" \
+#   --load-env "" \
+#   --num-nodes 1 \
+#   --num-gpus 8 \
+#   --partition learnfair \
+#   --external-redis
 
 import os
 import argparse
