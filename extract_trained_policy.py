92554c4366d7e5f6bc3c3a85a22762a6c640407a
theophilee <theophile.gervet@gmail.com>
2022-08-30 09:55:39 +0900

    dev

---
+++ extract_trained_policy.py
@@ -0,0 +1,10 @@
+from ray.rllib.agents import ppo
+
+from submission.env_wrapper.semexp_policy_training_env_wrapper import SemanticExplorationPolicyTrainingEnvWrapper
+
+
+algo = ppo.PPO(
+    config=ppo.DEFAULT_CONFIG.copy(),
+    env=SemanticExplorationPolicyTrainingEnvWrapper
+)
+algo.restore("~/ray_results/ddppo_overfit_challenge/DDPPO_SemanticExplorationPolicyTrainingEnvWrapper_7d280_00000_0_2022-08-28_14-29-25/checkpoint_000400/checkpoint-400")
