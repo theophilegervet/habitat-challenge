752eddf8ee8622986a07d14565f1b275596077cb
theophilee <theophile.gervet@gmail.com>
2022-08-30 10:44:57 +0900

    dev

--- extract_trained_policy.py
+++ extract_trained_policy.py
@@ -5,7 +5,10 @@ from submission.env_wrapper.semexp_policy_training_env_wrapper import SemanticEx
 
 config, config_str = get_config("submission/configs/debug_config.yaml")
 ppo_config = ppo.DEFAULT_CONFIG.copy()
-ppo_config.update({"env_config": {"config": config}})
+ppo_config.update({
+    "env_config": {"config": config},
+    "num_gpus_per_worker": 1
+})
 algo = ppo.PPOTrainer(
     config=ppo_config,
     env=SemanticExplorationPolicyTrainingEnvWrapper
