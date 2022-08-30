6bc5c1ac41a464c9cea29bc4a197f153878ee5be
theophilee <theophile.gervet@gmail.com>
2022-08-30 11:23:24 +0900

    dev

--- extract_trained_policy.py
+++ extract_trained_policy.py
@@ -88,7 +88,7 @@ checkpoint_path = "/private/home/theop123/ray_results/ddppo_overfit_challenge/DD
 with open(checkpoint_path, 'rb') as f:
     checkpoint = pickle.load(f)
 
-print(checkpoint["model"].keys())
+print(checkpoint.keys())
 
 # value = model["worker"]
 # weights=pickle.loads(value)
