a41ab6e656fb2d3b265870a9fd35268c7a4af997
theophilee <theophile.gervet@gmail.com>
2022-08-30 10:52:09 +0900

    dev

--- extract_trained_policy.py
+++ extract_trained_policy.py
@@ -3,12 +3,69 @@ from ray.rllib.agents import ppo
 from submission.utils.config_utils import get_config
 from submission.env_wrapper.semexp_policy_training_env_wrapper import SemanticExplorationPolicyTrainingEnvWrapper
 
+
 config, config_str = get_config("submission/configs/debug_config.yaml")
+
+
+# ppo_config = ppo.DEFAULT_CONFIG.copy()
+# ppo_config.update({
+#     "env_config": {"config": config},
+#     "num_gpus_per_worker": 1
+# })
+
+
+local_map_size = (
+        config.AGENT.SEMANTIC_MAP.map_size_cm //
+        config.AGENT.SEMANTIC_MAP.global_downscaling //
+        config.AGENT.SEMANTIC_MAP.map_resolution //
+        config.AGENT.POLICY.SEMANTIC.inference_downscaling
+    )
+map_features_shape = (
+    config.ENVIRONMENT.num_sem_categories + 8,
+    local_map_size,
+    local_map_size
+)
 ppo_config = ppo.DEFAULT_CONFIG.copy()
 ppo_config.update({
+    "env": SemanticExplorationPolicyTrainingEnvWrapper,
     "env_config": {"config": config},
-    "num_gpus_per_worker": 1
+    "model": {
+        "custom_model": "semantic_exploration_policy",
+        "custom_model_config": {
+            "map_features_shape": map_features_shape,
+            "hidden_size": 256,
+            "num_sem_categories": config.ENVIRONMENT.num_sem_categories,
+        }
+    },
+    "gamma": config.TRAIN.RL.gamma,
+    "lr": config.TRAIN.RL.lr,
+    "entropy_coeff": config.TRAIN.RL.entropy_coeff,
+    "clip_param": config.TRAIN.RL.clip_param,
+    "framework": "torch",
+    # "disable_env_checking": True,
+    "_disable_preprocessor_api": True,
+    "ignore_worker_failures": True
+    # "recreate_failed_workers": True,
 })
+ppo_config.update({
+    # Workers
+    "num_workers": config.TRAIN.RL.PPO.num_workers,
+    "num_gpus": config.TRAIN.RL.PPO.num_gpus,
+    "num_cpus_for_driver": config.TRAIN.RL.PPO.num_cpus_for_driver,
+    "num_gpus_per_worker": config.TRAIN.RL.PPO.num_gpus_per_worker,
+    "num_cpus_per_worker": config.TRAIN.RL.PPO.num_cpus_per_worker,
+    # Batching
+    #   train_batch_size: total batch size
+    #   sgd_minibatch_size: SGD minibatch size (chunk train_batch_size
+    #    in sgd_minibatch_size sized pieces)
+    "rollout_fragment_length": config.TRAIN.RL.PPO.rollout_fragment_length,
+    "train_batch_size": (config.TRAIN.RL.PPO.rollout_fragment_length *
+                         config.TRAIN.RL.PPO.num_workers),
+    "sgd_minibatch_size": 2 * config.TRAIN.RL.PPO.rollout_fragment_length,
+    "num_sgd_iter": config.TRAIN.RL.PPO.sgd_epochs,
+})
+
+
 algo = ppo.PPOTrainer(
     config=ppo_config,
     env=SemanticExplorationPolicyTrainingEnvWrapper
