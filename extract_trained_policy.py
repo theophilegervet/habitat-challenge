134574484f2c42ae67d51242585b4ed5a94f4ddb
theophilee <theophile.gervet@gmail.com>
2022-08-30 11:32:21 +0900

    dev

--- extract_trained_policy.py
+++ extract_trained_policy.py
@@ -45,6 +45,7 @@ ppo_config.update({
             "num_sem_categories": config.ENVIRONMENT.num_sem_categories,
         },
     },
+    "num_gpus_per_worker": 1,
     # "gamma": config.TRAIN.RL.gamma,
     # "lr": config.TRAIN.RL.lr,
     # "entropy_coeff": config.TRAIN.RL.entropy_coeff,
