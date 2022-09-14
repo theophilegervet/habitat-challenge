2344854f67c498dfaeb4ed7525d899d29e7f94f2
theophilee <theophile.gervet@gmail.com>
2022-09-14 14:15:35 -0400

    dev

--- debug.py
+++ debug.py
@@ -1,4 +1,5 @@
 from submission.env_wrapper.semexp_policy_training_env_wrapper import SemanticExplorationPolicyTrainingEnvWrapper
+from submission.dataset.semexp_policy_training_dataset import SemanticExplorationPolicyTrainingDataset
 from submission.utils.config_utils import get_config
 
 
@@ -6,4 +7,5 @@ config, config_str = get_config("submission/configs/ddppo_train_custom_annotated
 config.defrost()
 config.TASK_CONFIG.DATASET.SPLIT = "val"
 config.freeze()
-env = SemanticExplorationPolicyTrainingEnvWrapper(config=config)
+dataset = SemanticExplorationPolicyTrainingDataset(config=config.TASK_CONFIG)
+# env = SemanticExplorationPolicyTrainingEnvWrapper(config=config)
