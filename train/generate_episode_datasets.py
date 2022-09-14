e06f055b190650104a03b479f1c5ba7d61181e3d
theophilee <theophile.gervet@gmail.com>
2022-09-14 10:41:11 -0400

    dev

--- train/generate_episode_datasets.py
+++ train/generate_episode_datasets.py
@@ -92,7 +92,7 @@ def generate_scene_episodes(scene_path: str,
     scene_dir = "/".join(scene_path.split("/")[:-1])
     scene_key = scene_path.split("/")[-1].split(".")[0]
     map_dir = scene_dir + f"/floor_semantic_maps_{semantic_map_type}"
-    with open(f"{map_dir}/{scene_key}_info.json", "w") as f:
+    with open(f"{map_dir}/{scene_key}_info.json", "r") as f:
         scene_info = json.load(f)
     scene_info["floor_maps"] = []
     for i in range(len(scene_info["floor_heights"])):
