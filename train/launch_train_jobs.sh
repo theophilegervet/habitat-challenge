dbdcadfd2f6c33163a317a994e8ef180f79b0be5
theophilee <theophile.gervet@gmail.com>
2022-09-28 07:39:13 -0400

    configs

---
+++ train/launch_train_jobs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Launch job on challenge dataset to make sure everything runs fine there
+python train/rllib_slurm_launch.py \
+    --exp-name ppo_overfit_challenge_hm3d \
+    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_overfit_challenge_hm3d_dataset_config.yaml" \
+    --load-env "" \
+    --num-nodes 1 \
+    --num-gpus 8 \
+    --partition learnfair \
+    --external-redis
+
+# Overfit semantic exploration policy on one scene of HM3D, MP3D, Gibson
+for DATASET in hm3d mp3d gibson
+do
+  python train/rllib_slurm_launch.py \
+    --exp-name ppo_overfit_custom_$DATASET_annotated_scenes \
+    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_overfit_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
+    --load-env "" \
+    --num-nodes 1 \
+    --num-gpus 8 \
+    --partition learnfair \
+    --external-redis
+done
+
+# Train semantic exploration policy on HM3D, MP3D, Gibson
+for DATASET in hm3d mp3d gibson
+do
+  python train/rllib_slurm_launch.py \
+    --exp-name ddppo_train_custom_$DATASET_annotated_scenes \
+    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ddppo_train_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
+    --load-env "" \
+    --num-nodes 8 \
+    --num-gpus 8 \
+    --partition learnfair \
+    --external-redis
+done
