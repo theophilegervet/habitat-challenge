cf9ad937c92580fca5c5190cfba59e60e5f73de5
theophilee <theophile.gervet@gmail.com>
2022-09-29 08:46:31 -0400

    Debug RLLib on Slurm

--- train/launch_train_jobs.sh
+++ train/launch_train_jobs.sh
@@ -23,12 +23,12 @@ do
     --external-redis
 done
 
-# Training jobs with a single node
+# Train semantic exploration policy on HM3D, MP3D, Gibson with PPO and 1 node
 for DATASET in hm3d mp3d gibson
 do
   python train/rllib_slurm_launch.py \
     --exp-name ddppo_train_custom_${DATASET}_annotated_scenes \
-    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ddppo_train_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
+    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_train_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
     --load-env "" \
     --num-nodes 1 \
     --num-gpus 8 \
@@ -36,7 +36,7 @@ do
     --external-redis
 done
 
-# Train semantic exploration policy on HM3D, MP3D, Gibson
+# Train semantic exploration policy on HM3D, MP3D, Gibson with DDPPO and 8 nodes
 #for DATASET in hm3d mp3d gibson
 #do
 #  python train/rllib_slurm_launch.py \
