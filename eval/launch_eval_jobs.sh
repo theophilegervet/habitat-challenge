d26d10c3730a972aead3b995d3e60891d914fe71
theophilee <theophile.gervet@gmail.com>
2022-10-02 10:02:40 -0400

    dev

--- eval/launch_eval_jobs.sh
+++ eval/launch_eval_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Confusion matrix on HM3D, MP3D, Gibson
-DATASETS = ("hm3d" "mp3d" "gibson")
-CHECKPOINTS = (
+DATASETS=("hm3d" "mp3d" "gibson")
+CHECKPOINTS=(
   "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000500/checkpoint-500"
   "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000200/checkpoint-200"
   "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000600/checkpoint-600"
