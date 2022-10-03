ff04933f6111bf38b4083fad6395038c5fa5240e
theophilee <theophile.gervet@gmail.com>
2022-10-03 08:38:15 -0400

    dev

--- eval/launch_eval_jobs.sh
+++ eval/launch_eval_jobs.sh
@@ -3,9 +3,9 @@
 # Confusion matrix on HM3D, MP3D, Gibson
 DATASETS=("hm3d" "mp3d" "gibson")
 CHECKPOINTS=(
-  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000500/checkpoint-500"
-  "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000200/checkpoint-200"
-  "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000600/checkpoint-600"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000800/checkpoint-800"
+  "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000300/checkpoint-300"
+  "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000900/checkpoint-900"
 )
 for i in ${!DATASETS[@]}; do
   for j in ${!DATASETS[@]}; do
--- train/launch_train_jobs.sh
+++ train/launch_train_jobs.sh
@@ -11,17 +11,17 @@
 #    --external-redis
 
 # Overfit semantic exploration policy on one scene of HM3D, MP3D, Gibson
-for DATASET in hm3d mp3d gibson
-do
-  python train/rllib_slurm_launch.py \
-    --exp-name ppo_overfit_custom_${DATASET}_annotated_scenes \
-    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_overfit_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
-    --load-env "" \
-    --num-nodes 1 \
-    --num-gpus 8 \
-    --partition learnfair # \
-    #--external-redis
-done
+#for DATASET in hm3d mp3d gibson
+#do
+#  python train/rllib_slurm_launch.py \
+#    --exp-name ppo_overfit_custom_${DATASET}_annotated_scenes \
+#    --command "python train/train_semantic_exploration_policy.py --config_path submission/configs/ppo_overfit_custom_${DATASET}_annotated_scenes_dataset_config.yaml" \
+#    --load-env "" \
+#    --num-nodes 1 \
+#    --num-gpus 8 \
+#    --partition learnfair # \
+#    #--external-redis
+#done
 
 # Train semantic exploration policy on HM3D, MP3D, Gibson with PPO and 1 node
 for DATASET in hm3d mp3d gibson
