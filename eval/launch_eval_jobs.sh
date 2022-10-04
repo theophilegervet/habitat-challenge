bbd0453c5abfdc018053845f9ba6670f57aad13d
theophilee <theophile.gervet@gmail.com>
2022-10-04 08:56:28 -0400

    dev

--- eval/launch_eval_jobs.sh
+++ eval/launch_eval_jobs.sh
@@ -2,8 +2,13 @@
 
 # Confusion matrix on HM3D, MP3D, Gibson
 DATASETS=("hm3d" "mp3d" "gibson")
+#CHECKPOINTS=(
+#  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000800/checkpoint-800"
+#  "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000300/checkpoint-300"
+#  "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000900/checkpoint-900"
+#)
 CHECKPOINTS=(
-  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000800/checkpoint-800"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001000/checkpoint-1000"
   "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000300/checkpoint-300"
   "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000900/checkpoint-900"
 )
