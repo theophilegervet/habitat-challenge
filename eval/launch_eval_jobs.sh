de16c53997f14bccd8f4a2db29eb04edb0761a3e
theophilee <theophile.gervet@gmail.com>
2022-10-10 15:40:01 -0400

    Find best HM3D ckpt

--- eval/launch_eval_jobs.sh
+++ eval/launch_eval_jobs.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
 # Confusion matrix on HM3D, MP3D, Gibson
-DATASETS=("hm3d" "mp3d" "gibson")
+#DATASETS=("hm3d" "mp3d" "gibson")
 #CHECKPOINTS=(
-#  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000800/checkpoint-800"
+#  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001000/checkpoint-1000"
 #  "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000300/checkpoint-300"
 #  "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000900/checkpoint-900"
 #)
+#for i in ${!DATASETS[@]}; do
+#  for j in ${!DATASETS[@]}; do
+#    sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_${DATASETS[$i]}_config.yaml \
+#      AGENT.POLICY.type semantic \
+#      AGENT.POLICY.SEMANTIC.checkpoint_path ${CHECKPOINTS[$j]} \
+#      EXP_NAME eval_${DATASETS[$j]}_on_${DATASETS[$i]}
+#  done
+#done
+
+# Eval different HM3D checkpoints
+CHECKPOINT_NAMES=("700" "800" "900" "1100" "1300" "1500")
 CHECKPOINTS=(
-  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001000/checkpoint-1000"
-  "/private/home/theop123/ray_results/ppo_train_mp3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcae0_00000_0_2022-10-01_08-00-07/checkpoint_000300/checkpoint-300"
-  "/private/home/theop123/ray_results/ppo_train_gibson_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_040bb_00000_0_2022-09-30_05-18-39/checkpoint_000900/checkpoint-900"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000700/checkpoint_700"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_bcfe1_00000_0_2022-09-30_03-00-40/checkpoint_000800/checkpoint_800"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_000900/checkpoint-900"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001100/checkpoint-1100"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001300/checkpoint-1300"
+  "/private/home/theop123/ray_results/ppo_train_hm3d_annotated_scenes/PPO_SemanticExplorationPolicyTrainingEnvWrapper_d3d73_00000_0_2022-10-03_11-40-18/checkpoint_001500/checkpoint-1500"
 )
-for i in ${!DATASETS[@]}; do
-  for j in ${!DATASETS[@]}; do
-    sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_${DATASETS[$i]}_config.yaml \
-      AGENT.POLICY.type semantic \
-      AGENT.POLICY.SEMANTIC.checkpoint_path ${CHECKPOINTS[$j]} \
-      EXP_NAME eval_${DATASETS[$j]}_on_${DATASETS[$i]}
-  done
+for i in ${!CHECKPOINTS[@]}; do
+  sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml \
+    AGENT.POLICY.type semantic \
+    AGENT.POLICY.SEMANTIC.checkpoint_path ${CHECKPOINTS[$j]} \
+    EXP_NAME eval_hm3d_ckpt_${CHECKPOINT_NAMES[$i]}
 done
 
 # Ablation study on HM3D
