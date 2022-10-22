7bc6ff58721cde197fb898ef465520cb2d0e52a1
theophilee <theophile.gervet@gmail.com>
2022-10-22 15:30:08 -0700

    dev

--- eval/extract_single_floor_episodes.py
+++ eval/extract_single_floor_episodes.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         episode for episode in env._dataset.episodes
         if len([
             goal for goal in episode.goals
-            if abs(episode.start_position[1] - goal.position[1]) < 1.0  # 1.5
+            if episode.start_position[1] - 0.25 < goal.position[1] < episode.start_position[1] + 1.5
         ]) > 0
     ]
     same_floor_episodes = len(env._dataset.episodes)
