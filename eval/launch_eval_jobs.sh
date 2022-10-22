d87693415ffd662654a2d24f0096fd000c2f3220
theophilee <theophile.gervet@gmail.com>
2022-10-22 15:33:25 -0700

    dev

--- eval/launch_eval_jobs.sh
+++ eval/launch_eval_jobs.sh
@@ -34,7 +34,7 @@
 #done
 
 # Ablation study on HM3D
-sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME eval_standard
-sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME eval_goal_on_same_floor EVAL_VECTORIZED.goal_on_same_floor 1
-sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME eval_gt_semantics GROUND_TRUTH_SEMANTICS 1
-sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME eval_goal_on_same_floor_and_gt_semantics EVAL_VECTORIZED.goal_on_same_floor 1 GROUND_TRUTH_SEMANTICS 1
+sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME new_eval_standard
+sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME new_eval_goal_on_same_floor EVAL_VECTORIZED.goal_on_same_floor 1
+sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME new_eval_gt_semantics GROUND_TRUTH_SEMANTICS 1
+sbatch eval/eval_vectorized.sh --config_path=submission/configs/eval_hm3d_config.yaml EXP_NAME new_eval_goal_on_same_floor_and_gt_semantics EVAL_VECTORIZED.goal_on_same_floor 1 GROUND_TRUTH_SEMANTICS 1
