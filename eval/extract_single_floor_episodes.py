75cd9effdfa62a386e02e19755e311941a01e72a
theophilee <theophile.gervet@gmail.com>
2022-11-01 21:10:57 -0400

    Eval on 1st floor episodes

--- eval/extract_single_floor_episodes.py
+++ eval/extract_single_floor_episodes.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 from pathlib import Path
+import json
 import sys
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
@@ -14,7 +15,6 @@ if __name__ == "__main__":
     env = Env(config=config.TASK_CONFIG)
     obs = env.reset()
 
-    # Optimal floor threshold seems to be around 1.5m
     episode_start_height_and_goal_heights = [
         (
             episode.start_position[1],
@@ -40,6 +40,16 @@ if __name__ == "__main__":
         ]) > 0
     ]
     same_floor_episodes = len(env._dataset.episodes)
+
+    first_floor_episodes = []
+    for episode in env._dataset.episodes:
+        scene_dir = "/".join(episode.scene_id.split("/")[:-1])
+        map_dir = scene_dir + "floor_semantic_maps_annotations_top_down"
+        scene_id = episode.scene_id.split("/")[-1].split(".")[0]
+        with open(f"{map_dir}/{scene_id}_info.json", "r") as f:
+            scene_info = json.load(f)
+            print(scene_info)
+
     print("Total episodes:", total_episodes)
     print("Same floor episodes:", same_floor_episodes)
     print(f"Different floor episodes {(total_episodes - same_floor_episodes) / total_episodes * 100:.2f}%")
